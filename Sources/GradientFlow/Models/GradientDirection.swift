0bb8fd24919003af9e9357b1c88f7759fbf695be
JaeWoong Eum <wodnd0418@gmail.com>
2026-01-22 16:07:16 +0900

    feat: add GradientDirection model

---
+++ Sources/GradientFlow/Models/GradientDirection.swift
@@ -0,0 +1,24 @@
+//
+//  File.swift
+//  GradientFlow
+//
+//  Created by wodnd on 1/22/26.
+//
+
+import Foundation
+
+/// Gradient direction
+public enum GradientDirection {
+    /// From left to right
+    case leftToRight
+    
+    /// From right to left
+    case rightToLeft
+    
+    /// From top to bottom
+    case topToBottom
+    
+    /// From bottom to top
+    case bottomToTop
+}
+
